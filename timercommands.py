7a8c482ce508e70a77efa6aff28559e43b910c13
N-010 <chelsig96@gmail.com>
2022-07-16 17:40:32 +0300

    Tick - 1

--- timercommands.py
+++ timercommands.py
@@ -27,6 +27,8 @@ class TimerCommands():
 
         self.__background_tasks = []
 
+        self.__functions:list = [self.__send_min_max, self.__send_tick, self.__send_revenues, self.__send_admin_scores, self.__send_scores]
+
     @staticmethod
     def get_utc():
         return datetime.utcnow().replace(second=0, microsecond=0)
@@ -69,6 +71,11 @@ class TimerCommands():
         def is_me(message: Message):
             return True or message.author == self.__bot.user
 
+        messages = await self.__tick_channel.history(limit=10).flatten()
+
+        if len(messages) <= len(self.__functions):
+            return
+
         while True:
             l = len(await self.__tick_channel.purge(check=is_me))
             print(l)
@@ -109,7 +116,7 @@ class TimerCommands():
         pretty_tick = []
         for data in tick_data[:10]:
             try:
-                tick = data["tick"]
+                tick = data["tick"] - 1
                 amount = data["amount"]
             except Exception as e:
                 logging.warning(e)
@@ -157,7 +164,7 @@ class TimerCommands():
         admin_scores = await get_admin_scores()
         if len(admin_scores) <= 0:
             logging.warning("__send_admin_scores: admin_scores is empty")
-        #     return
+            return
 
         pretty_data = admin_scores_pretty(admin_scores)
         file_name = prepare_file_name('admin_scores.txt')
@@ -189,16 +196,14 @@ class TimerCommands():
 
         file_name = prepare_file_name('scores.txt')
         message: Message = await self.__get_last_file_message("scores")
-        print(message)
         await self.__send_edit_file_message(message, file_name, f"{os.linesep}".join(scores))
         
 
     async def loop(self):
         while True:
-            await pool.add_command(self.__send_min_max)
-            await pool.add_command(self.__send_tick)
-            await pool.add_command(self.__send_revenues)
-            await pool.add_command(self.__send_admin_scores)
-            await pool.add_command(self.__send_scores)
+            tasks = []
+            for function in self.__functions:
+                tasks.append(asyncio.create_task(function()))
 
-            await asyncio.sleep(60)
+            tasks.append(asyncio.create_task(asyncio.sleep(60)))
+            await asyncio.wait(tasks)
